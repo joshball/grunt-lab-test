783e6f12682933f30d4d2902489701cab409744e
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-11-14 11:51:19 -0800

    Adding some dummy tests

---
+++ tests/test.coverage.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var it = lab.it;
+
+
+describe('COVERAGE Tests', function () {
+    describe('TEST TEST', function () {
+        it('should BE TRUE', function (done) {
+            expect(true).to.be.true;
+            done();
+        });
+    });
+});
+
